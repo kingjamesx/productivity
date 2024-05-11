7b4946a40a40fe4e25ad2fba81892be57c4ce925
Hikmot-O <hikmotokunola@gmail.com>
2024-05-11 09:37:05 +0100

    mobile fix

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Logo = () => {
   return (
-    <div className="pl-11 font-bold text-xl flex items-center gap-1">
-      <div className="w-5 h-5 bg-blue-500 rounded-bl-[7px] rounded-tr-[4px]"></div>
+    <div className="pl-11 font-bold text-lg md:text-xl flex items-center gap-1">
+      <div className="w-3.5 h-3.5 md:w-5 md:h-5 bg-blue-500 rounded-bl-[7px] rounded-tr-[4px]"></div>
       PunchGoal
     </div>
   );
